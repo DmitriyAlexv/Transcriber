4fec643fd24cc3994dd86b0d357df402a30b3c75
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-12-16 07:23:25 +0300

    add audio process settings

--- Transcriber/Transcriber.Client.Desktop/ViewModels/SettingDetails/AudioProcessSettingDetailsViewModel.cs
+++ Transcriber/Transcriber.Client.Desktop/ViewModels/SettingDetails/AudioProcessSettingDetailsViewModel.cs
@@ -1,5 +1,7 @@
 using System.Reactive;
 using ReactiveUI;
+using Transcriber.Client.Desktop.Models;
+using Transcriber.Client.Desktop.Services;
 using Transcriber.Client.Desktop.ViewModels.Abstractions;
 using Transcriber.Client.Desktop.ViewModels.Controls.SettingsView;
 
@@ -7,11 +9,37 @@ namespace Transcriber.Client.Desktop.ViewModels.SettingDetails;
 
 public class AudioProcessSettingDetailsViewModel: ViewModelBase, IHaveTitle
 {
+    private AudioProcessSettings _audioProcessSettings;
+    
     public string Title => "Обработка";
+    
+    public AudioProcessSettings Settings
+    {
+        get => _audioProcessSettings;
+        set => this.RaiseAndSetIfChanged(ref _audioProcessSettings, value);
+    }
     public ReactiveCommand<Unit, Unit> NavigateBackCommand { get; }
     
     public AudioProcessSettingDetailsViewModel(SettingsNavigationViewModel settingsNavigationViewModel)
     {
-        NavigateBackCommand = ReactiveCommand.Create(settingsNavigationViewModel.NavigateBack);
+        Settings = Copy(Singleton.AppSettingsManager.AudioProcessSettings);
+        
+        NavigateBackCommand = ReactiveCommand.Create(() => 
+        {
+            Singleton.AppSettingsManager.AudioProcessSettings = Copy(Settings);
+            settingsNavigationViewModel.NavigateBack();
+        });
+    }
+
+    private AudioProcessSettings Copy(AudioProcessSettings audioProcessSettings)
+    {
+        return new AudioProcessSettings
+        {
+            PackageSize = audioProcessSettings.PackageSize,
+            DueTime = audioProcessSettings.DueTime,
+            MaxPeriod = audioProcessSettings.MaxPeriod,
+            MinPeriod = audioProcessSettings.MinPeriod,
+            DataProcessOptions = audioProcessSettings.DataProcessOptions,
+        };
     }
 }
